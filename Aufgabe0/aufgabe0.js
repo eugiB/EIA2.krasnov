4ebe1442318cb0c098dc7683ca0828bf8bf0f2b8
eugiB <43846342+eugiB@users.noreply.github.com>
2018-10-06 16:16:56 +0200

    Add files via upload

---
+++ Aufgabe0/aufgabe0.js
@@ -0,0 +1,18 @@
+/* Aufgabe: Aufgabe 0
+Name: Eugen Krasnov
+Matrikel: 259525
+Datum: 05.10.2018
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
+Er wurde nicht kopiert und auch nicht diktiert.
+*/
+var n = "";
+function text() {
+    var n = prompt("Wie lautet dein Name?");
+    var node = document.getElementById("xD");
+    node.innerHTML += "Hey ";
+    node.innerHTML += n;
+    node.innerHTML += "Was geht?";
+    console.log("Hey ", n, "Was geht?");
+}
+document.addEventListener('DOMContentLoaded', text);
+//# sourceMappingURL=aufgabe0.js.map
---
+++ Aufgabe0/aufgabe0.ts
@@ -0,0 +1,21 @@
+/* Aufgabe: Aufgabe 0
+Name: Eugen Krasnov 
+Matrikel: 259525
+Datum: 05.10.2018
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. 
+Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
+
+    var n : string = "";
+
+    function text() {
+        var n = prompt ("Wie lautet dein Name?");
+        var node = document.getElementById("xD");
+        node.innerHTML += "Hey ";
+        node.innerHTML += n;
+        node.innerHTML += "Was geht?";
+        console.log("Hey ", n, "Was geht?");
+        }
+    
+        document.addEventListener('DOMContentLoaded', text);
