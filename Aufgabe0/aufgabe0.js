a0e73f7c03025765eea9a62994a8eecb3daf148c
eugiB <43846342+eugiB@users.noreply.github.com>
2018-10-06 14:51:51 +0200

    Add files via upload
    Abgabe Aufgabe0

---
+++ Aufgabe0/aufgabe0.js
@@ -0,0 +1,21 @@
+/* Aufgabe: Aufgabe 0
+Name: Eugen Krasnov
+Matrikel: 259525
+Datum: 05.10.2018
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
+Er wurde nicht kopiert und auch nicht diktiert.
+*/
+function text1() {
+    var n = "";
+    function text() {
+        var n = prompt("Wie lautet dein Name?");
+        var node = document.getElementById("xD");
+        node.innerHTML += "Hey ";
+        node.innerHTML += n;
+        node.innerHTML += ", was geht?";
+        
+        console.log("Hey ", n, "was geht?");
+    }
+    document.addEventListener('DOMContentLoaded', text1);
+}
+//# sourceMappingURL=aufgabe0.js.map
